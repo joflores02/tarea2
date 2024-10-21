8a195edd29e253595881cce94d8b1aa74624d4db
CrissIHT <115413143+CrissIHT@users.noreply.github.com>
2024-10-21 02:43:35 +0100

    ClaseFinalEmpleado.java

--- tareaJUnit/src/main/java/Empleado.java
+++ tareaJUnit/src/main/java/Empleado.java
@@ -1,9 +1,20 @@
+/**
+ * Clase que representa un empleado de la empresa.
+ */
 public class Empleado {
     private String id;
     private String apellido;
     private String nombre;
     private String correo;
 
+    /**
+     * Constructor para crear un nuevo empleado.
+     *
+     * @param id       El identificador del empleado.
+     * @param nombre   El nombre del empleado.
+     * @param apellido El apellido del empleado.
+     * @param correo   El correo electrónico del empleado.
+     */
     public Empleado(String id, String nombre, String apellido, String correo) {
         this.id = id;
         this.nombre = nombre;
@@ -11,40 +22,80 @@ public class Empleado {
         this.correo = correo;
     }
 
-    public String getId(){
+    /**
+     * Obtiene el identificador del empleado.
+     *
+     * @return El ID del empleado.
+     */
+    public String getId() {
         return id;
     }
 
+    /**
+     * Obtiene el nombre del empleado.
+     *
+     * @return El nombre del empleado.
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * Obtiene el apellido del empleado.
+     *
+     * @return El apellido del empleado.
+     */
     public String getApellido() {
         return apellido;
     }
 
+    /**
+     * Obtiene el correo electrónico del empleado.
+     *
+     * @return El correo del empleado.
+     */
     public String getCorreo() {
         return correo;
     }
 
+    /**
+     * Establece el nombre del empleado.
+     *
+     * @param nombre El nuevo nombre del empleado.
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    /**
+     * Establece el apellido del empleado.
+     *
+     * @param apellido El nuevo apellido del empleado.
+     */
     public void setApellido(String apellido) {
         this.apellido = apellido;
     }
 
+    /**
+     * Establece el identificador del empleado.
+     *
+     * @param id El nuevo ID del empleado.
+     */
     public void setId(String id) {
         this.id = id;
     }
 
+    /**
+     * Establece el correo electrónico del empleado.
+     *
+     * @param correo El nuevo correo del empleado.
+     */
     public void setCorreo(String correo) {
         this.correo = correo;
     }
 
     @Override
     public String toString() {
-        return id+ " "+ nombre + " " +apellido+" "+ correo;
+        return id + " " + nombre + " " + apellido + " " + correo;
     }
 }
