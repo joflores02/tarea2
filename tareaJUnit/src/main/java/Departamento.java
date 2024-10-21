bc2c3e7fc69be42464d64b0fef48702fdc5e9ada
CrissIHT <115413143+CrissIHT@users.noreply.github.com>
2024-10-21 02:42:46 +0100

    ClaseFinalDepartamento.java

--- tareaJUnit/src/main/java/Departamento.java
+++ tareaJUnit/src/main/java/Departamento.java
@@ -1,30 +1,64 @@
 import java.util.ArrayList;
 import java.util.List;
+
+/**
+ * Clase que representa un departamento en la empresa.
+ */
 public class Departamento {
     private String nombreDepartamento;
     private List<Empleado> empleados;
 
-    public Departamento(String nombreDepartamento){
+    /**
+     * Constructor para crear un nuevo departamento.
+     *
+     * @param nombreDepartamento El nombre del departamento.
+     */
+    public Departamento(String nombreDepartamento) {
         this.nombreDepartamento = nombreDepartamento;
         empleados = new ArrayList<>();
-
     }
+
+    /**
+     * Agrega un empleado al departamento.
+     *
+     * @param empleado El empleado a agregar.
+     */
     public void agregarEmpleado(Empleado empleado) {
         empleados.add(empleado);
     }
 
+    /**
+     * Obtiene la lista de empleados del departamento.
+     *
+     * @return La lista de empleados.
+     */
     public List<Empleado> getEmpleados() {
         return empleados;
     }
 
+    /**
+     * Obtiene la cantidad de empleados en el departamento.
+     *
+     * @return El número de empleados.
+     */
     public int obtenerCantidadEmpleados() {
         return empleados.size();
     }
 
-    public String getNombreDepartamento(){
+    /**
+     * Obtiene el nombre del departamento.
+     *
+     * @return El nombre del departamento.
+     */
+    public String getNombreDepartamento() {
         return nombreDepartamento;
     }
 
+    /**
+     * Establece el nombre del departamento.
+     *
+     * @param nombreDepartamento El nuevo nombre del departamento.
+     */
     public void setNombreDepartamento(String nombreDepartamento) {
         this.nombreDepartamento = nombreDepartamento;
     }
@@ -33,6 +67,4 @@ public class Departamento {
     public String toString() {
         return "Departamento: " + nombreDepartamento + "\nEmpleados: " + empleados;
     }
-
-
 }
